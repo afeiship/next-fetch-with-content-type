c47d6fe198786a8abd720daa90ee20d8d32d9ee6
afeiship <1290657123@qq.com>
2023-03-14 23:12:33 +0800

    fix: from 2 get json

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -2,8 +2,9 @@ import weibo2res from '../src';
 
 describe('api.basic', () => {
   test('parse baisc data when sucess', () => {
-    const input = `<meta http-equiv="Content-Type"content="text/html; charset=utf-8"/><script type="text/javascript">document.domain="sina.com.cn";</script>
-{"code":"A00006","data":{"count":1,"data":"eyJ1aWQiOjM2NjE4MzI4MDMsImFwcCI6Im1pbmlibG9nIiwiY291bnQiOjEsInRpbWUiOjE2Nzg4MDA5OTUuMTIsInBpY3MiOnsicGljXzEiOnsid2lkdGgiOjE1MDQsInNpemUiOjE2MTY0NSwicmV0IjoxLCJoZWlnaHQiOjQwMCwibmFtZSI6InBpY18xIiwicGlkIjoiZGE0MzIyNjNneTFoYnpwdG9mdnA0ajIxNXMwYjR0Y3QifX19","pics":{"pic_1":{"width":1504,"size":161645,"ret":1,"height":400,"name":"pic_1","pid":"da432263gy1hbzptofvp4j215s0b4tct"}}}}`;
+    const input = `    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <script type="text/javascript">document.domain="sina.com.cn";</script>
+{"code":"A00006","data":{"count":1,"data":"eyJ1aWQiOjM2NjE4MzI4MDMsImFwcCI6Im1pbmlibG9nIiwiY291bnQiOjEsInRpbWUiOjE2Nzg4MDY2MjQuODgsInBpY3MiOnsicGljXzEiOnsid2lkdGgiOjE1MDQsInNpemUiOjE2MTY0NSwicmV0IjoxLCJoZWlnaHQiOjQwMCwibmFtZSI6InBpY18xIiwicGlkIjoiZGE0MzIyNjNneTFoYnpzamIxcnVnajIxNXMwYjR0Y3QifX19","pics":{"pic_1":{"width":1504,"size":161645,"ret":1,"height":400,"name":"pic_1","pid":"da432263gy1hbzsjb1rugj215s0b4tct"}}}}`;
     const res = weibo2res(input);
     expect(res).toEqual({
       code: 'A00006',
@@ -12,7 +13,7 @@ describe('api.basic', () => {
       ret: 1,
       height: 400,
       name: 'pic_1',
-      pid: 'da432263gy1hbzptofvp4j215s0b4tct',
+      pid: 'da432263gy1hbzsjb1rugj215s0b4tct',
     });
   });
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export default (inWeiboResponse: string) => {
   if (typeof inWeiboResponse !== 'string') return inWeiboResponse;
-  const json2str = inWeiboResponse.split('\n')[1];
+  const json2str = inWeiboResponse.split('\n')[2];
   const data = JSON.parse(json2str);
   const target = data.data?.pics?.pic_1;
   return {
